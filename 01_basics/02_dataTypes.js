51e408c712365a1de0ceebe4317e462bed9a2331
riisav147 <98591906+riisav147@users.noreply.github.com>
2024-02-11 10:05:39 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict" // treat all js code as newer version;
+// alert(3+3)//this will not work because we are using nodejs not browser;
+
+//number => 2 to the power 53
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
